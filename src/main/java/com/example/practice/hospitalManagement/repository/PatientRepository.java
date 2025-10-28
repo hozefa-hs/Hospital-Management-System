0d83a4623773721d8a3b8b93b3676313d4051e76
Hozefa <hozefasailanawala786@gmail.com>
2025-10-29 00:23:28 +0530

    Add additional methods to PatientRepository and corresponding tests
    - Implemented methods for finding patients by birth date, counting blood group types, and pagination support.
    
    - Added tests for new repository methods in PatientTests.

--- src/main/java/com/example/practice/hospitalManagement/repository/PatientRepository.java
+++ src/main/java/com/example/practice/hospitalManagement/repository/PatientRepository.java
@@ -1,7 +1,47 @@
 package com.example.practice.hospitalManagement.repository;
 
 import com.example.practice.hospitalManagement.entity.Patient;
+import com.example.practice.hospitalManagement.entity.type.BloodGroupType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Patient findByName(String name);
+
+    List<Patient> findByEmail(String mail);
+
+    List<Patient> findByBirthDateOrEmail(LocalDate birthDate, String email);
+
+    List<Patient> findByNameContaining(String name);
+
+
+    @Query("select p from Patient p where p.bloodGroup = ?1")
+    List<Patient> findByBloodGroup(@Param("bloodGroup") BloodGroupType bloodGroupType);
+
+    @Query("select p from Patient p where p.birthDate > :birthDate")
+    List<Patient> findByBornAfterDate(@Param("birthDate") LocalDate birthDate);
+
+    @Query("select p.bloodGroup, Count(p) from Patient p group by p.bloodGroup")
+    List<Object[]> countEachBloodGroupType();
+
+    @Query(value = "select * from patient_table", nativeQuery = true)
+    List<Patient> findAllPatients();
+
+    //Pagination Support
+    @Query(value = "select * from patient_table", nativeQuery = true)
+    Page<Patient> findAllPatientsWithPagination(Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Patient p SET p.name = :name where p.id = :id")
+    int updateNameWithId(@Param("name") String name, @Param("id") Long id);
+
 }
--- src/test/java/com/example/practice/hospitalManagement/PatientTests.java
+++ src/test/java/com/example/practice/hospitalManagement/PatientTests.java
@@ -1,11 +1,15 @@
 package com.example.practice.hospitalManagement;
 
 import com.example.practice.hospitalManagement.entity.Patient;
+import com.example.practice.hospitalManagement.entity.type.BloodGroupType;
 import com.example.practice.hospitalManagement.repository.PatientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootTest
@@ -15,9 +19,45 @@ public class PatientTests {
     private PatientRepository patientRepository;
 
     @Test
-    public void testPatientRepository(){
+    public void testPatientRepository() {
         List<Patient> patientList = patientRepository.findAll();
         System.out.println(patientList);
+    }
+
+    @Test
+    public void testPatientRepositoryMethods() {
+
+//        Patient patient = patientRepository.findByName("John Doe");
+
+//        List<Patient> patientList = patientRepository.findByBirthDateOrEmail(LocalDate.of(1985, 7, 15), "sarah.wilson@example.com");
+
+//        List<Patient> patientList = patientRepository.findByNameContaining("J");
+
+//        List<Patient> patientList = patientRepository.findByBloodGroup(BloodGroupType.A_POSITIVE);
+
+//        List<Patient> patientList = patientRepository.findByBornAfterDate(LocalDate.of(1991, 2, 15));
+
+//        List<Patient> patientList = patientRepository.findAllPatients();
+//        for (Patient patient : patientList) {
+//            System.out.println("Hello" + patient);
+//        }
+
+        //Pagination Support
+        System.out.println("Pagination");
+        Page<Patient> patientPage = patientRepository.findAllPatientsWithPagination(PageRequest.of(0, 2));
+        for (Patient patient : patientPage) {
+            System.out.println(patient);
+        }
+
+
+        List<Object[]> list = patientRepository.countEachBloodGroupType();
+        for (Object[] objects : list) {
+//            System.out.println(objects[0] + " " + objects[1]);
+        }
+
+        int updatedRows = patientRepository.updateNameWithId("Hozefa", 1L);
+        System.out.println(updatedRows);
+
     }
 
 }
